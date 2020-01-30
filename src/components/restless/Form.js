7f3d3532273c2fc0986b2154886d520f0bb42613
Jacob Harrington <jharrington4@gmail.com>
2020-01-30 14:42:30 -0800

    Basic form completed

---
+++ src/components/restless/Form.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Form = ({ url, method, body, onSubmit, onChange }) => {
+  return (
+    <section>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="url" placeholder="URL" value={url} onChange={onChange} />
+        <section>
+          <div>
+            <label>
+              <input type="radio" name="method" value="GET" checked={method === 'GET'} onChange={onChange} />
+              GET
+            </label>
+            <label>
+              <input type="radio" name="method" value="POST" checked={method === 'POST'} onChange={onChange} />
+              POST
+            </label>
+            <label>
+              <input type="radio" name="method" value="PUT" checked={method === 'PUT'} onChange={onChange}/ >
+              PUT
+            </label> 
+            <label>
+              <input type="radio" name="method" value="PATCH" checked={method === 'PATCH'} onChange={onChange} /> 
+              PATCH
+            </label>
+            <label>
+              <input type="radio" name="method" value="DELETE" checked={method === 'DELETE'} onChange={onChange} />
+              DELETE
+            </label>
+          </div>
+          <button>Go!</button>
+        </section>
+        <textarea name="body" value={body} onChange={onChange}></textarea>
+      </form>
+    </section>
+  );
+};
+
+Form.propTypes = {
+  url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default Form;
--- src/containers/Restless.js
+++ src/containers/Restless.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/restless/Header';
+import Form from '../components/restless/Form';
 
 export default class Restless extends Component{
 
   render(){
     return (
-      <Header/>
+      <>
+        <Header/>
+        <Form />
+      </>
     );
   }
 }
