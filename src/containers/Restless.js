65d5a33e32bcb8a0e59ba801c05cdf2eb1dae91a
Jacob Harrington <jharrington4@gmail.com>
2020-01-30 14:19:27 -0800

    added basic Header and Container for app

---
+++ src/containers/Restless.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../components/restless/Header';
+
+export default class Restless extends Component{
+
+  render(){
+    return (
+      <Header/>
+    );
+  }
+}
