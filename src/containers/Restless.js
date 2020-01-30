f067519fc77b032a8d9cf1613051728948d2d6d8
Jacob Harrington <jharrington4@gmail.com>
2020-01-30 14:48:40 -0800

    form state and onChange handler created

--- src/containers/Restless.js
+++ src/containers/Restless.js
@@ -3,12 +3,33 @@ import Header from '../components/restless/Header';
 import Form from '../components/restless/Form';
 
 export default class Restless extends Component{
+  state ={
+    url: '',
+    method: '',
+    body: '',
+    display: ''
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  handleSubmit =({ target }) => {
+    
+  }
 
   render(){
+    const { url, method, body } = this.state;
+
     return (
       <>
         <Header/>
-        <Form />
+        <Form 
+          url={url} 
+          method={method} 
+          body={body} 
+          onSubmit={this.handleSubmit} 
+          onChange={this.handleChange} />
       </>
     );
   }
