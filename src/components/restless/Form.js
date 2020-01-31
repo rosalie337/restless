7a9190856adaa3dab4bd57e5435f475b442303a0
Jacob Harrington <jharrington4@gmail.com>
2020-01-30 19:51:39 -0800

    finished form styling

--- src/components/restless/Form.js
+++ src/components/restless/Form.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Form.css';
 
 const Form = ({ url, method, body, onSubmit, onChange }) => {
   return (
     <section>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="url" placeholder="URL" value={url} onChange={onChange} />
+      <form onSubmit={onSubmit} className={styles.Form}>
+        <input type="text" className={styles.url} name="url" placeholder="URL" value={url} onChange={onChange} />
         <section>
           <div>
-            <label>
-              <input type="radio" name="method" value="GET" checked={method === 'GET'} onChange={onChange} />
-              GET
-            </label>
-            <label>
-              <input type="radio" name="method" value="POST" checked={method === 'POST'} onChange={onChange} />
-              POST
-            </label>
-            <label>
-              <input type="radio" name="method" value="PUT" checked={method === 'PUT'} onChange={onChange}/ >
-              PUT
-            </label> 
-            <label>
-              <input type="radio" name="method" value="PATCH" checked={method === 'PATCH'} onChange={onChange} /> 
-              PATCH
-            </label>
-            <label>
-              <input type="radio" name="method" value="DELETE" checked={method === 'DELETE'} onChange={onChange} />
-              DELETE
-            </label>
+            
+            <input type="radio" id="get" name="method" value="GET" checked={method === 'GET'} onChange={onChange} />
+            <label htmlFor="get">GET</label>
+            
+            <input type="radio" id="post" name="method" value="POST" checked={method === 'POST'} onChange={onChange} />
+            <label htmlFor="post">POST</label>
+            
+            <input type="radio" id="put" name="method" value="PUT" checked={method === 'PUT'} onChange={onChange}/ >
+            <label htmlFor="put">PUT</label> 
+            
+            <input type="radio" id="patch" name="method" value="PATCH" checked={method === 'PATCH'} onChange={onChange} /> 
+            <label htmlFor="patch">PATCH</label>
+            
+            <input type="radio" id="delete" name="method" value="DELETE" checked={method === 'DELETE'} onChange={onChange} />
+            <label htmlFor="delete">DELETE</label>
           </div>
           <button>Go!</button>
         </section>
